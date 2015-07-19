905e8930cc896910e9225989187bb0e5ff6cc4cc
ShunsukeFurubayashi <shunsuke_furubayashi@dwango.co.jp>
2015-07-19 19:57:57 +0900

    Modify paging view

--- DemoApp/PagingCollectionView/PagingCollectionView.swift
+++ DemoApp/PagingCollectionView/PagingCollectionView.swift
@@ -10,12 +10,86 @@ import UIKit
 
 class PagingCollectionView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+    // MARK: - Properties
+    internal let collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
+    internal var contentSize: CGSize {
+        return CGSizeMake(CGRectGetWidth(UIScreen.mainScreen().bounds) - 30, CGRectGetWidth(UIScreen.mainScreen().bounds) - 30)
     }
-    */
+    
+    // MARK: - Init
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.initView()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initView()
+    }
+    
+    private func initView() {
+        
+        self.collectionView.backgroundColor = self.backgroundColor
+        self.collectionView.frame = CGRectMake(15.0, 15.0, self.contentSize.width, self.contentSize.height)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        self.collectionView.showsHorizontalScrollIndicator = false
+        self.collectionView.showsVerticalScrollIndicator = false
+        self.collectionView.clipsToBounds = false
+        self.collectionView.pagingEnabled = true
+        self.collectionView.alwaysBounceHorizontal = true
+        // For Cell
+        self.collectionView.registerNib(UINib(nibName: "PagingCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "PagingCollectionViewCell")
+        
+        // Change layout
+        let layout = PagingCollectionViewLayout()
+        layout.scrollDirection = .Horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        self.collectionView.setCollectionViewLayout(layout, animated: false)
+        
+        self.addSubview(self.collectionView)
+    }
+
+    // MARK: - Override Methods
+    override func hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
+        if !CGRectContainsPoint(self.collectionView.frame, point) {
+            return self.collectionView
+        }
+        return super.hitTest(point, withEvent: event)
+    }
+    
+}
+
+// MARK: - UICollectionView DataSource
+extension PagingCollectionView: UICollectionViewDataSource {
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PagingCollectionViewCell", forIndexPath: indexPath) as! PagingCollectionViewCell
+        
+        return cell
+    }
+    
+}
+
+// MARK: - UICollectionView DataSource
+extension PagingCollectionView: UICollectionViewDelegate {
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+         return self.contentSize
+    }
 
 }
--- DemoApp/PagingCollectionView/PagingCollectionViewLayout.swift
+++ DemoApp/PagingCollectionView/PagingCollectionViewLayout.swift
@@ -10,5 +10,19 @@ import UIKit
 
 class PagingCollectionViewLayout: UICollectionViewFlowLayout {
  
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [AnyObject]? {
+        
+        // 左右１ピクセルをはみ出させることで、普通は描画されない横のスライドも描画させる
+        // このことで、カードリストのような左右のcellがframe外で表示されるようになる
+        // ただし、collectionView本体のclipsToBoundsがfalseになっていないといけない
+        let attributes = super.layoutAttributesForElementsInRect(rect)
+        for attribute in attributes as! [UICollectionViewLayoutAttributes] {
+            var rect = attribute.bounds
+            rect.size.width += 1
+            attribute.bounds = rect
+        }
+        
+        return attributes
+    }
     
 }
